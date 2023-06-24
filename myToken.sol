ffee3cffe506b60d0cdbb80eca3d6b31bf1e23c4
Aron Nick Santos <91038206+aronnicksnts@users.noreply.github.com>
2023-06-24 08:11:56 +0800

    Implementation of assert(), revert(), and require() added

--- myToken.sol
+++ myToken.sol
@@ -1,40 +1,49 @@
+
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.17;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
-
-contract MyToken {
-
-    // public variables here
+contract CHESS {
+   // public variables here
    string public tokenName = "CHESS";
    string public tokenAbbrv = "CHS";
    uint public totalSupply = 0;
-    // mapping variable here
-   mapping(address=>uint) public balances;
 
-    // mint function
-   function mint(address _address, uint _value) public {
-      totalSupply += _value;
-      balances[_address] += _value;
+   address public minter;
+   mapping(address => uint) public balances;
+
+
+   event Sent(address from, address to, uint amount);
+
+   constructor() {
+      minter = msg.sender;
    }
 
-    // burn function
-   function burn(address _address, uint _value) public {
-      if (balances[_address] >= _value){
-         totalSupply -= _value;
-         balances[_address] -= _value;
-      }
+   function mint(address receiver, uint amount) public {
+      require(msg.sender == minter);
+      balances[receiver] += amount;
+      totalSupply += amount;
    }
+
+   error InsufficientBalance(uint requested, uint available);
+
+   function send(address receiver, uint amount) public {
+      if (amount > balances[msg.sender])
+      revert InsufficientBalance({
+         requested: amount,
+         available: balances[msg.sender]
+      });
+
+      balances[msg.sender] -= amount;
+      balances[receiver] += amount;
+
+      emit Sent(msg.sender, receiver, amount);
+   }
+
+   function burn(uint amount) public {
+      require(balances[minter] >= amount);
+      balances[minter] -= amount;
+      totalSupply -= amount;
+   }
+
 }
